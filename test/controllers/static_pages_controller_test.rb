7377b1486c7d76c3ba03a7e01b5f6f521e4061dc
Kevin <supergoodblog@gmail.com>
2015-02-10 14:01:43 -0600

    lowers test repetition

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+  	@base_title = "protoApp"
+  end
+
   test "should get home page" do
     get :home
     assert_response :success
-    assert_select "title","protoApp | home"
+    assert_select "title","#{@base_title} | home"
   end
 
   test "should get help page" do
     get :help
     assert_response :success
-    assert_select "title","protoApp | help"
+    assert_select "title","#{@base_title} | help"
   end
 
   test "should get about page" do
   	get :about
   	assert_response :success
-  	assert_select "title","protoApp | about"
+  	assert_select "title","#{@base_title} | about"
   end
 
 end
