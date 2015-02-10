fb30f24ee067277f9e3c422664922b3e5df22221
Kevin <supergoodblog@gmail.com>
2015-02-10 13:16:20 -0600

    passed tests for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "should get home page" do
     get :home
     assert_response :success
+    assert_select "title","protoApp | home"
   end
 
-  test "should get help" do
+  test "should get help page" do
     get :help
     assert_response :success
+    assert_select "title","protoApp | help"
+  end
+
+  test "should get about page" do
+  	get :about
+  	assert_response :success
+  	assert_select "title","protoApp | about"
   end
 
 end
