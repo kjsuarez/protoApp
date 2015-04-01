dbabd24e0adb3c24ff255aeee2637fb36d801fe2
Kevin <supergoodblog@gmail.com>
2015-03-31 22:27:34 -0400

    used sessions to add login/logout capability for users and changed the header layout accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'  
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
 
 
